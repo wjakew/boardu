78b4a0e69de27ffa10474abcff3340a6dc13188c
Jakub Wawak <34348257+wjakew@users.noreply.github.com>
2022-09-08 15:45:44 +0200

    Initial commit

---
+++ src/components/Note.js
@@ -0,0 +1,17 @@
+import { MdDeleteForever } from 'react-icons/md';
+
+const Note = () => {
+    return(
+
+        <div className="note">
+            <span> Hello! This is our first note! Hura!</span>
+            <div className = "note-footer">
+                <small>13/04/2021</small>
+                <MdDeleteForever className = 'delete-icon' size='1.3em' />
+            </div>
+
+        </div>
+
+    )
+}
+export default Note;
---
+++ src/components/NotesList.js
@@ -0,0 +1,18 @@
+import Note from './Note';
+
+const NotestList = () => {
+    return (
+        <div className = "notes-list"> 
+           
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        
+        
+        </div>
+    )
+}
+
+export default NotestList;
